6d01e9fac5341ed68ccf032aff040eb480bccfc6
m-sakano@bfts.co.jp <m-sakano@bfts.co.jp>
2015-11-11 15:53:21 +0900

    CSS

--- login.php
+++ login.php
@@ -15,14 +15,14 @@
     <title><?php echo BRAND;?></title>
 
     <!-- Bootstrap core CSS -->
-    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/docs/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="cover.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="bootstrap/dist/assets/js/ie-emulation-modes-warning.js"></script>
+    <script src="bootstrap/docs/dist/assets/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
